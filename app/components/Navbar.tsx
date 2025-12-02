a9c8a57cbc8d02c7d40e4587c8d53ec91e6d9788
TryTanOP <aryanisop9@gmail.com>
2025-12-03 04:20:13 +0530

    Refactor Navbar component to reset body overflow style correctly when menu is closed.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -18,10 +18,10 @@ export default function Navbar() {
     if (isMenuOpen) {
       document.body.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = "unset";
+      document.body.style.overflow = "";
     }
     return () => {
-      document.body.style.overflow = "unset";
+      document.body.style.overflow = "";
     };
   }, [isMenuOpen]);
 
